26e5d39e9d5b930c72edea6e76f4a10442f592fd
Felipe1218 <pipe.r6@hotmail.com>
2018-05-18 00:57:02 -0500

    creacion login con devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+	def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :tipo_documento, :numero_documento, :nombre, :telefono, :celular, :direccion, :ciudad, :departamento, :pais, :profesion])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :tipo_documento, :numero_documento, :nombre, :telefono, :celular, :direccion, :ciudad, :departamento, :pais, :profesion])
+    end
 end
